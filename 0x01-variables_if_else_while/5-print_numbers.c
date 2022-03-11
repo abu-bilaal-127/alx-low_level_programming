6ffb75f66bbf1b5179ccdc39915819a237780e36
abubilall <m.abdulrofihi@gmail.com>
2022-03-11 21:37:24 +0100

    Task 5 - Numbers.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description - prints the decimal digits (0..9)
+ *
+ * Return: 0
+**/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+}
