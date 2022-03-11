a83ece029ee11219ef5092141f1925f0db4745d9
abubilall <m.abdulrofihi@gmail.com>
2022-03-11 04:58:35 +0100

    Tasks 4, 6 & 100.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  *
  * Description - prints "Programming is like building a multilingual puzzle"with a newline.
  *
- * Return value: 0
+ * Returns - 0
  **/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
