36c7789bb8cb84916d029a48db16c221ec372fb3
abubilall <m.abdulrofihi@gmail.com>
2022-03-16 17:55:29 +0100

    Task 11 - Battery Street.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* print_to_98 - prints natural numbers from n-98
+*
+* @n: integer n
+*
+* Return: void
+**/
+void print_to_98(int n)
+{
+	for(n; n < 98; n++)
+	{
+		printf("%d, ");
+	}
+}
