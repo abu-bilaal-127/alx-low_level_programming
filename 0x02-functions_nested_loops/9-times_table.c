92c338d5785fa84c7de90357f139115f8efde5ad
abubilall <m.abdulrofihi@gmail.com>
2022-03-16 17:36:32 +0100

    Task 9 - Times table.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,14 +1,24 @@
 #include "main.h"
 
+/**
+* times_table - multiplication table function
+*
+* Description - prints mulitiplication table 1-9
+*
+* Return: void
+**/
 void times_table(void)
 {
-	int multiplier = 0;
+	int multiplier, number;
+
+	mulitplier = 0;
 
 	while (multiplier < 10)
 	{
-		for (int number = 0; number < 10; number++)
+		for (number = 0; number < 10; number++)
 		{
 			int result = number * multiplier;
+
 			_putchar(result);
 			_putchar('\n');
 		}
