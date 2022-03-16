92d99c65057e9e27b5f46152f299d218a64f264b
abubilall <m.abdulrofihi@gmail.com>
2022-03-16 14:18:18 +0100

    Task 1 - Alphabet (fixed).

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,21 @@
 #include "main.h"
 
+/**
+* print_alphabet - a void function
+*
+* Description - prints the english alphabets
+*
+* Return: void
+**/
 void print_alphabet(void)
 {
 	int counter = 97;
 
-	while(counter < 122)
+	while (counter < 123)
 	{
 		_putchar(counter);
 		counter++;
 	}
-	
+
 	_putchar('\n');
 }
