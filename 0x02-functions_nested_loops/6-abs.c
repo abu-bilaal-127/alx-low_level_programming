1bed554b94df5f816daa5a9af8e6e2d96d110f28
abubilall <m.abdulrofihi@gmail.com>
2022-03-16 16:46:26 +0100

    Task 6 - Estimate.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _abs - absolute value
+*
+* @int - integer value
+*
+* Description - computes absolute value of an int
+*
+* Return - 0
+**/
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int *= -1;
+	}
+	else
+	{
+		int *= 1;
+	}
+
+	return(0);
+}
