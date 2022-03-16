1a982e8375ad4134004cbab0c971975dbc63ccc5
abubilall <m.abdulrofihi@gmail.com>
2022-03-16 17:02:31 +0100

    Task 7 - Colors, digits and notes.

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_last_digit - prints last digit
+*
+* @n: integer parameter
+*
+* Description - prints last digit of a number
+*
+* Return: integer
+**/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
