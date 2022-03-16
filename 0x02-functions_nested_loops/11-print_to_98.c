989a64b990bc3abfc40eb905025e746002c0767d
abubilall <m.abdulrofihi@gmail.com>
2022-03-16 21:15:02 +0100

    Task 11 - Battery street (fixed).

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,27 @@
 **/
 void print_to_98(int n)
 {
-	while (n < 99)
+	if (n < 99)
 	{
-		printf("%d, ", n);
-		n++;
+		while (n < 99)
+		{
+			printf("%d, ", n);
+			n++;
+		}
 	}
+	else if (n > 99)
+	{
+		while (n > 99)
+		{
+			printf("%d, " n);
+			n--;
+		}
+	}
+	else
+	{
+		printf("%d\n", n);
+		break;
+	}
+
+	printf("98\n");
 }
