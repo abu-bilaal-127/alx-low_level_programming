4e12e7d44cd53d3bee7f3e9071fedf4430b510b3
abubilall <m.abdulrofihi@gmail.com>
2022-03-17 09:46:04 +0100

    Task 3 - The numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_numbers - prints 0-9
+*
+* Description - print 0-9
+*
+* Return: 0
+**/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
