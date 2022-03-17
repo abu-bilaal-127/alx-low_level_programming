895a7507db9eea01907dafa01ecf7f74517cbfa4
abubilall <m.abdulrofihi@gmail.com>
2022-03-17 09:52:39 +0100

    Task 4 - Numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints 0-9 except 2 and 4
+*
+* Description - prints 0-9 except 2 and 4
+*
+* Return: void
+**/
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+
+	_putchar('\n');
+}
