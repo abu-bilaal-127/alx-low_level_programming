485c2a0f1760130cd066768ad945e6aecedc12f7
abubilall <m.abdulrofihi@gmail.com>
2022-03-22 03:54:43 +0100

    Task 1 - Don't swap horses (betty fixed).

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
-* swap_int - swaps the values of two integers
-* Description - swaps the values of two integers
-* @*a: pointer variable to an integer
-* @*b: pointer variable to an integer
-* @swap: integer variable
+* swap_int - exchanges the values of two integers
+* Description - exchanges the values of two integers
+* @a: pointer variable to an integer
+* @b: pointer variable to an integer
 * Return: void
 **/
 void swap_int(int *a, int *b)
