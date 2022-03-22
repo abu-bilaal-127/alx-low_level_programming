87e9ea235d20c3539e56e64bdc5bc9a01e7221f3
abubilall <m.abdulrofihi@gmail.com>
2022-03-22 07:20:17 +0100

    Task 4 - No reverse gear (fixed).

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,18 +8,18 @@
 **/
 void print_rev(char *s)
 {
-	int total_index, rev_index;
+	int counter, rev_index;
 
-	total_index = 0;
+	counter = 0;
 	rev_index = -1;
 
 	while (*(s + counter) != 0)
 		counter++;
 
-	while (total_index != rev_index)
+	while (counter != rev_index)
 	{
-		_putchar(s[total_index]);
-		total_index--;
+		_putchar(s[counter]);
+		counter--;
 	}
 
 	_putchar('\n');
