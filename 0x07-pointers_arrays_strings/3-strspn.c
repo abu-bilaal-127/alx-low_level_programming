748096fae0ec3094c0680f0dd5fecc8730cf57e1
abubilall <m.abdulrofihi@gmail.com>
2022-03-28 17:12:35 +0100

    Task 3 - strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+* _strspn - gets the length of a prefix substring
+* @s: pointer variable to the string
+* @accept: pointer variable to the source
+* Return: number of bytes
+**/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	unsigned int prev = 0;
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+		prev = count;
+		while (*(accept + j) != '\0')
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				count++;
+			}
+			j++;
+		}
+		if (prev == count)
+		{
+			break;
+		}
+		i++;
+	}
+
+	return (count);
+}
