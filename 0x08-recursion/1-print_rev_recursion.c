cb073b28c95a9924a6e5bda047db094f72d84589
abubilall <m.abdulrofihi@gmail.com>
2022-03-31 23:22:17 +0100

    Task 1 - Dream

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
 /**
-* _print_rev_recursion - prints a string in reverse
-* @s: string variable
-* Return: void
-**/
+*_print_rev_recursion - function that prints a string in reverse
+*@s: parameter passed
+*Return: return null
+*/
 void _print_rev_recursion(char *s)
 {
+
 	if (*s != '\0')
+
 	{
-		_print_rev_recursion((s + 1));
-		_putchar(*(s + 0));
+
+		_print_rev_recursion(s + 1);
+
+		_putchar(*s);
+
 	}
+
 }
